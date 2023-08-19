88d52a993a9e0d3383e9baf0559d23c99267f8e1
Brais Fernández <braisoncrece@gmail.com>
2023-08-19 11:48:35 +0200

    feat: depth first search algorithm

--- algorithms/recursion/binary_tree_tools.rb
+++ algorithms/recursion/binary_tree_tools.rb
@@ -1,4 +1,14 @@
-class BinaryTreeTools
+# frozen_string literal: true
+
+class BinaryTreeTools # rubocop:disable Style/Documentation
+  def self.depth_first_search(node:, value:)
+    return false unless node
+    return true if node.value == value
+
+    return depth_first_search(node: node.right, value: value) if node.value < value
+    return depth_first_search(node: node.left, value: value) if node.value >= value
+  end
+
   def self.breath_first_search(head:, needle:)
     bfs(head: head, needle: needle)
   end
@@ -15,53 +25,58 @@ class BinaryTreeTools
     post_order_walk(current_node: head, path: [])
   end
 
-  def self.compare(a:, b:)
-    return true if a == nil && b == nil
-    return false if a == nil || b == nil
-    return false if a.value != b.value
+  def self.compare(first:, second:)
+    return true if first.nil? && second.nil?
+    return false if first.nil? || second.nil?
+    return false if first.value != second.value
 
-    return compare(a: a.left, b: b.left) && compare(a: a.right, b: b.right)
+    compare(first: first.left, second: second.left) && compare(first: first.right, second: second.right)
   end
 
   private_class_method def self.pre_order_walk(current_node:, path:)
-                         return path unless current_node
-                         path << current_node.value
-                         pre_order_walk(current_node: current_node.left, path: path)
-                         pre_order_walk(current_node: current_node.right, path: path)
-                         path
-                       end
+    return path unless current_node
+
+    path << current_node.value
+    pre_order_walk(current_node: current_node.left, path: path)
+    pre_order_walk(current_node: current_node.right, path: path)
+    path
+  end
 
   private_class_method def self.in_order_walk(current_node:, path:)
-                         return path unless current_node
-                         in_order_walk(current_node: current_node.left, path: path)
-                         path << current_node.value
-                         in_order_walk(current_node: current_node.right, path: path)
-                         path
-                       end
+    return path unless current_node
+
+    in_order_walk(current_node: current_node.left, path: path)
+    path << current_node.value
+    in_order_walk(current_node: current_node.right, path: path)
+    path
+  end
 
   private_class_method def self.post_order_walk(current_node:, path:)
-                         return path unless current_node
-                         post_order_walk(current_node: current_node.left, path: path)
-                         post_order_walk(current_node: current_node.right, path: path)
-                         path << current_node.value
-                         path
-                       end
+    return path unless current_node
+
+    post_order_walk(current_node: current_node.left, path: path)
+    post_order_walk(current_node: current_node.right, path: path)
+    path << current_node.value
+    path
+  end
 
   private_class_method def self.bfs(head:, needle:)
-                         return false unless head
-                         queue = [head]
-                         until queue.empty?
-                           current = queue.shift
-                           return true if current.value == needle
-                           queue << current.left if current.left
-                           queue << current.right if current.right
-                         end
-                         # if we don't find the value
-                         false
-                       end
+    return false unless head
+
+    queue = [head]
+    until queue.empty?
+      current = queue.shift
+      return true if current.value == needle
+
+      queue << current.left if current.left
+      queue << current.right if current.right
+    end
+    # if we don't find the value
+    false
+  end
 end
 
-class Node
+class Node # rubocop:disable Style/Documentation
   attr_accessor :value, :left, :right
 
   def initialize(value:, left: nil, right: nil)
